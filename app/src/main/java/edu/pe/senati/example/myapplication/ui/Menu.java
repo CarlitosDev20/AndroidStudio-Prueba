faf0fea1831c6ff7f1ec20aea4edc21401f1e467
CarlitosDev20 <cghv21304@gmail.com>
2025-10-14 13:54:21 -0500

    Avance de proyecto el dia 14/10/2025

---
+++ app/src/main/java/edu/pe/senati/example/myapplication/ui/Menu.java
@@ -0,0 +1,44 @@
+package edu.pe.senati.example.myapplication.ui;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.pe.senati.example.myapplication.R;
+import edu.pe.senati.example.myapplication.databinding.FragmentMenuBinding;
+
+public class Menu extends Fragment {
+    FragmentMenuBinding binding;
+    View view;
+    Context context;
+    NavController navController;
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentMenuBinding.inflate(inflater, container,false);
+        return view = binding.getRoot();
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view,savedInstanceState);
+        context = getContext();
+        navController = Navigation.findNavController(view);
+    }
+
+}
